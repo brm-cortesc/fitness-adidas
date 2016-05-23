b4c8f03b507b77c928121d5132450ef7968a3130
Cristian Cortés <cristian.cortes@brm.com.co>
2016-05-23 15:21:53 -0500

    first init

---
+++ js/fitness-adidas.ini.js
@@ -0,0 +1,165 @@
+var btnColor = $('.color'),
+	btnTalla = $('.talla'),
+	camiseta = $('#camiseta'),
+	selectCamiseta = $('#selector-camiseta'),
+	animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+	input = $('input.form-control'),
+	idCamiseta = selectCamiseta.val();
+
+jQuery(document).ready(function($) {
+
+	/*Validacion de campos de formulario*/
+	$("#registro").validate({
+
+		       // debug: true,
+
+		      /*Contenedor y clase donde se pinta el error*/
+		      errorElement: "div",
+		      errorClass: "alert-danger",
+
+		      /*Campos para validar en form para pedir fiesta*/
+
+		    rules: {
+		           multi1:          {required: true},  
+		           multi2:          {required: true},  
+		           multi3:          {required: true},  
+		           nombres:       {required: true, accept: "[a-zA-Z]+" },
+		           apellidos:       {required: true, accept: "[a-zA-Z]+" },
+		           email:       {required: true, email: true},  
+		           telefono:          {required: true, digits: true},  
+		           genero:        {required: true},
+		           departamento:        {required: true},
+		           ciudad:        {required: true},
+		           tipodoc:          {required: true},  
+		           documento:          {required: true, digits: true},  
+		           nacimiento:          {required: true},  
+		           autorizo:        {required: true},
+		           terminos:        {required: true},
+		           },
+
+		    /*Mensajes en caso de dar error para cada input*/
+
+		    messages: {
+		      multi1:      {required: "debes ingresar el número de lote"},
+		      multi2:      {required: "debes ingresar el número de lote"},
+		      multi3:      {required: "debes ingresar el número de lote"},
+		      nombres:      {required: "debes ingresar tu nombre", accept: "solo ingresa texto"},
+		      apellidos:      {required: "debes ingresar tus apellidos", accept: "solo ingresa texto"},
+		      email:      {required: "debes ingresar un email", email: "Ingresa un email válido"},
+		      telefono:  {required: "Indíca un número de teléfono", digits: "solo se aceptan números" },
+		      genero:         {required: "Indica un género"},
+		      departamento:         {required: "debes seleccionarndica un departamento"},
+		      ciudad:      {required: "debes seleccionar la ciudad"},
+		      tipodoc:  {required: "Indíca el tipo de documento" },
+		      documento:  {required: "Indíca un número de documento", digits: "solo se aceptan números" },
+		      nacimiento:         {required: "Debes indicar tu fecha de nacimiento"},
+		      autorizo:         {required: "Debes aceptar la política de datos"},
+		      terminos:         {required: "Debes aceptar los términos y condiciones de la actividad"}
+		     
+
+		           },
+
+		    errorPlacement: function (error, element) {
+
+		      if( element.attr('name') == 'terminos' || element.attr('name') == 'autorizo'){
+
+		        error.insertAfter(element.parent());
+
+		      }else{
+
+		        error.insertAfter(element);
+		      }
+
+		    }
+
+
+		});
+
+
+	/*boton de color activo*/
+	
+	btnColor.click(function(event) {
+		
+		event.preventDefault();
+
+		btnColor.removeClass('color-active');
+
+		$(this).addClass('color-active');
+
+	});
+
+	/*boton de talla activo*/
+
+	btnTalla.click(function(event) {
+		
+		event.preventDefault();
+
+		btnTalla.removeClass('talla-active');
+
+		$(this).addClass('talla-active');
+
+	});
+
+
+	/*cambio de camiseta*/
+	/*set default de camiseta*/
+	camiseta.attr('src', 'images/camisetas/'+idCamiseta+'.png?w=340&amp;ch=DPR&amp;dpr=1');
+	
+	camiseta.attr('data-zoom', 'images/camisetas/'+idCamiseta+'.png?w=1000&amp;ch=DPR&amp;dpr=1');
+
+	selectCamiseta.change(function() {
+
+		var color = $(this).find(':selected').attr('data-color');
+
+		idCamiseta = $(this).val();
+
+		camiseta.addClass('animated zoomOut');
+
+
+		camiseta.one(animationEnd, function () {
+			
+			$(this).removeClass('zoomOut');
+			camiseta.attr('src', 'images/camisetas/'+idCamiseta+'.png?w=340&amp;ch=DPR&amp;dpr=1');
+			camiseta.attr('data-zoom', 'images/camisetas/'+idCamiseta+'.png?w=1000&ch=DPR&dpr=2');
+			camiseta.addClass('animated zoomIn');
+
+		} );
+
+
+
+	});
+
+
+	/*animacion label formulario*/
+	input.focus( function() {
+
+		$(this).parent().addClass('active');
+
+
+	});
+	input.focusout(function() {
+		$(this).parent().removeClass('active');
+
+		if ( $(this).val() != '' ){
+
+		$(this).parent().addClass('active');
+			
+		}
+
+	});
+
+
+	/*funcion de zoom*/
+
+	setTimeout(function() {
+		new Drift(document.querySelector('#camiseta'), {
+		  paneContainer: document.querySelector('.detalle-camiseta'),
+			 inlinePane: true,
+			  containInline: true,
+			  zoomFactor: 3
+		});
+
+
+	}, 1000);
+
+});
